0693ba6a195ec88720b6b9ca96812830da783fc2
LHackSRT <bambabar02@gmail.com>
2025-05-21 16:26:14 +0000

    ...

---
+++ src/main/java/sn/zeitune/olive_insurance_gateway_service/OliveInsuranceGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package sn.zeitune.olive_insurance_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveInsuranceGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliveInsuranceGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/zeitune/olive_insurance_gateway_service/OliveInsuranceGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.zeitune.olive_insurance_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveInsuranceGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
